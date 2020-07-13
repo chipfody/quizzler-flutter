b8d47518462e8dd3954f278cd0c0165f44a037e2
chipfody <60615532+chipfody@users.noreply.github.com>
2020-07-13 13:54:18 -0400

    Added Questions

--- lib/question.dart
+++ lib/question.dart
@@ -1,12 +1,9 @@
 class Question {
-
   String questionText;
   bool questionAnswer;
 
-  Question ({String q, bool a}) {
-    questionText = q;
-    questionAnswer = a;
+  Question(String q, bool a) {
+    this.questionText = q;
+    this.questionAnswer = a;
   }
-
-
 }
